7df263f23817657d38337045ff1266bd4b58e03d
29veljace <raphael.kadainka@gmx.at>
2026-09-21 19:56:11 +0200

    added two new filters + filter switcher

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+mode = 0
+
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    manipulated_frame = cv2.bitwise_not(frame)
+    if mode == 0:
+        output = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+    elif mode == 1:
+        output = cv2.bitwise_not(frame)
+    else:
+        output = frame
 
-    cv2.imshow('Manipulated Webcam', manipulated_frame)
+    cv2.imshow('Manipulated Webcam', output)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('n'):
+        mode = (mode + 1) % 3
+    elif key == ord('q'):
         break
 
 cap.release()
