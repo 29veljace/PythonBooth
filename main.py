3a55fa0491730a40dca4fdd71a2fe4ecf98ad023
29veljace <raphael.kadainka@gmx.at>
2026-09-22 18:45:18 +0200

    added blur and pencil filters

--- main.py
+++ main.py
@@ -17,14 +17,18 @@ while True:
         output = frame
     elif mode == 3:
         output = cv2.applyColorMap(frame, cv2.COLORMAP_HSV)
-    else:
+    elif mode == 4:
         output = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    elif mode == 5:
+        output = cv2.GaussianBlur(frame, (15, 15), 0)
+    else:
+        output = cv2.Canny(frame, 100, 200)
 
     cv2.imshow('Manipulated Webcam', output)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('n'):
-        mode = (mode + 1) % 5
+        mode = (mode + 1) % 6
     elif key == ord('q'):
         break
 
